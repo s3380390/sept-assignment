1176a23d0a7d1fad6dcd7f3fd74e27f824b66a2a
Matthew Williams <s3380390@student.rmit.edu.au>
2017-03-22 13:44:14 +1100

    Added unit test
    Not passing on JSON append

--- www/lib/JSONHandler.php
+++ www/lib/JSONHandler.php
@@ -2,33 +2,40 @@
 class JSONHandler {
 	
 	public function getFileContents($filename){
+		
+		#check that file exists else throws an exception
+		if (!file_exists($filename))
+			throw new Exception($filename . " does not exist.");
+		
 		$contents = file_get_contents($filename);
 		
 		$JSONArray = json_decode($contents, TRUE);
+		
+		#checks to see if json_decode returns anything, else throws a format exception
+		if (!$JSONArray)
+			throw new Exception($filename . " is not in correct JSON format.");
 		
 		return $JSONArray;
 	}
 	
 	public function addToFile($filename, $data) {
 		$json = json_encode($data);
-		$sucess;
 		
 		if ($json)
-			$sucess = file_put_contents($filename, $json, FILE_APPEND);
+			file_put_contents($filename, $json, FILE_APPEND);
 		
-		if ($success)
-			return TRUE;
-		else
-			return FALSE;
 	}
 	
 	public function search($filename, $targetfield, $targetString){
 		$arr = $this->getFileContents($filename);
 		
+		#array search which returns the index of the value in which was found
 		$val = array_search($targetString, array_column($arr,$targetfield));
-
+		
+		if (!$val)
+			return NULL;
+		
 		return $arr[$val];
 	}
-	
 }
 ?>
---
+++ www/tests/JSONHandlerTest.php
@@ -0,0 +1,110 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require_once("../lib/JSONHandler.php");
+
+class JSONHandlerTest extends TestCase
+{
+	protected $testarr;
+	protected $testfile;
+	protected $jh;
+	
+	public  function setUp() 
+	{
+		$this->jh = new JSONHandler;
+		$this->testfile = 'testfile.json';
+		$this->testarr = array(
+		(0) => array
+			(
+				('name') => 'John',
+				('surname') => 'Smith'
+			),
+		(1) => array
+			(
+				('name') => 'Jenny',
+				('surname') => 'Brown'
+			)
+		);
+		
+		$this->jh->addToFile( $this->testfile, $this->testarr);
+	}
+	
+	public function tearDown() 
+	{
+		unlink(realpath($this->testfile));
+	}	
+	
+    public function testCreate()
+	{	
+		$this->jh->addToFile( 'createtest.json', $this->testarr);
+		$this->assertFileExists('createtest.json');
+		unlink(realpath('createtest.json'));
+	}
+	
+	public function testRead()
+	{
+		$this->testarr = $this->jh->getFileContents($this->testfile);
+		
+		$this->assertEquals($this->testarr[0]['name'], 'John');
+		$this->assertEquals($this->testarr[0]['surname'], 'Smith');
+		$this->assertEquals($this->testarr[1]['name'], 'Jenny');
+		$this->assertEquals($this->testarr[1]['surname'], 'Brown');
+	}
+	
+	public function testMissingFileRead()
+	{
+		$this->expectException(Exception::class);
+		$this->expectExceptionMessage('missing.json does not exist.');
+		
+		$missingtest = $this->jh->getFileContents('missing.json');
+	}
+	
+	public function testIncorrectFileRead()
+	{
+		$this->expectException(Exception::class);
+		$this->expectExceptionMessage('incorrect.json is not in correct JSON format.');
+		
+		$json = '[ { "nm: "Edmund lronside", "cty": "United Kingdom, "hse": "House of Wessex", "yrs": "1016" }, ';
+		file_put_contents('incorrect.json', $json);
+
+		$incorrecttest = $this->jh->getFileContents('incorrect.json');
+	}
+	
+	#Not passing append, needs another function, corrupts json
+	public function testAppend()
+	{
+		$this->testarr = array(
+		(0) => array
+			(
+				('name') => 'Jason',
+				('surname') => 'Orange'
+			)
+		);
+		
+		$this->jh->addToFile( $this->testfile, $this->testarr);
+		
+		$this->testarr = $this->jh->getFileContents($this->testfile);
+		
+		$this->assertEquals($this->testarr[0]['name'], 'John');
+		$this->assertEquals($this->testarr[2]['name'], 'Jason');
+	}
+	
+	public function testSearch()
+	{
+		$this->testarr= $this->jh->search($this->testfile, 'name', 'Jenny');
+		
+		$this->assertEquals($this->testarr['name'], 'Jenny');
+		$this->assertEquals($this->testarr['surname'], 'Brown');
+	}
+	
+	public function testSearchNotFound()
+	{
+		$this->testarr= $this->jh->search($this->testfile, "name", "Ben");
+		$this->assertEquals($this->testarr, NULL);
+	}
+	
+	public static function tearDownAfterClass()
+	{
+		unlink(realpath('incorrect.json'));
+	}
+}
+?>
