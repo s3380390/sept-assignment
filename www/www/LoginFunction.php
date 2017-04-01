d6da0829adc2c35602cd354b54ef2a0dbf3cddc8
rmit-s3524307-Quang-Anh-Nguyen <s3524307@student.rmit.edu.au>
2017-04-01 18:04:10 +1100

    Add files via upload
    Can now go to owner or customer home based on pwd

--- www/www/LoginFunction.php
+++ www/www/LoginFunction.php
@@ -3,20 +3,29 @@ session_start();
 include_once("../lib/JSONHandler.php");
 	
 	$l = new LoginFunctions;
-	if($l->login($_POST["username"], $_POST["password"])){
+	if($l->login($_POST["username"], $_POST["password"], "customer")){
 		header("Location: CustomerHome.html");
 	} else {
-		header("Location: login.html");
+		if ($l->login($_POST["username"], $_POST["password"], "owner")){
+			header("Location: OwnerHome.html");
+		}else{
+			header("Location: login.html");
+		}
 	}
 class LoginFunctions{
-	public function login($username, $password){
+	public function login($username, $password, $type){
 		$j = new JSONHandler;
-		$customersdb = "../database/customers.json";
-		if (!empty($user = $j->search($customersdb, "username", $username))){
+		$db = array("customer"=>"../database/customers.json", "owner"=>"../database/owners.json");
+		if (!empty($user = $j->search($db[$type], "username", $username))){
 			if ($user["password"] == $password){
 				$_SESSION["UserName"] = $user["username"];
 				$_SESSION["Password"] = $user["password"];
-				$_SESSION["Name"] = $user["name"];
+				if ($type == "customer"){
+					$_SESSION["Name"] = $user["name"];
+				} else {
+					$_SESSION["BusinessName"] = $user["businessname"];
+					$_SESSION["OwnerName"] = $user["ownername"];
+				}
 				$_SESSION["Address"] = $user["address"];
 				$_SESSION["Phone"] = $user["phoneno"];
 				return true;
