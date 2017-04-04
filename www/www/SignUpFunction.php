c5de1169c7e4ccee8346b7f6be1a35afe4a6ef39
rmit-s3524307-Quang-Anh-Nguyen <s3524307@student.rmit.edu.au>
2017-04-04 19:27:56 +1000

    Add files via upload
    fixed errors

--- www/www/SignUpFunction.php
+++ www/www/SignUpFunction.php
@@ -2,15 +2,25 @@
 	session_start();
 	include("../lib/JSONHandler.php");
 	
-	$l = new SignupFunctions;
-	if($l->signup($_POST["firstname"], $_POST["lastname"], $_POST["username"], $_POST["password1"], 
-				$_POST["password2"], $_POST["contact"], $_POST["address"])==true){
-		header("Location: SignUpConfirmation.html");		
+	$pattern = "/(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9])(?=.*[a-z])(?!.*[<>()?.,\/\\{}\[\\s]+\-~|;:\'\"=]).{6,}/";
+	if (preg_match($pattern, $_POST["password1"])){
+		if ($_POST["password1"]==$_POST["password2"]){
+			$l = new SignupFunctions;
+			if($l->signup($_POST["firstname"], $_POST["lastname"], $_POST["username"], $_POST["password1"], 
+			$_POST["password2"], $_POST["contact"], $_POST["address"])==true){
+				header("Location: SignUpConfirmation.html");		
+			} else {
+				$_SESSION["sameUserName"] = true;
+				header("Location: register.html");
+			}		
+		} else {
+			$_SESSION["samePwd"] = false;
+			header("Location: register.html");
+		}
 	} else {
-		$_SESSION["sameUserName"] = true;
+		$_SESSION["format"] = false;
 		header("Location: register.html");
 	}
-	
 class SignupFunctions{
 	public function signup($firstname, $lastname, $username, $password1, $password2, $contact, $address){
 		$j = new JSONHandler;
