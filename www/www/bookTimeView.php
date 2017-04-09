b6bf5b99561566efee533e3a044c0c226d4db624
rmit-s3524307-Quang-Anh-Nguyen <s3524307@student.rmit.edu.au>
2017-04-09 22:55:46 +1000

    Add files via upload
    fix the source code look

--- www/www/bookTimeView.php
+++ www/www/bookTimeView.php
@@ -7,20 +7,29 @@ $v = new View($db);
 class View{
 	public function view($db){
 		$j = new JSONHandler;
+		$f_tab = "\t\t";
+		$s_tab = "\t\t\t";
+		$t_tab = "\t\t\t\t";
+		$newline = "\r\n";
 		if (!empty($viewArray = $j->getFileContents($db))){
 			foreach ($viewArray as $employee){
-				echo "<p>{$employee["name"]} from {$employee["company"]}<p><table>";
-				echo "<th>Day</th> <th>Shift</th> <th>Working</th> <th>Booked</th>\r\n";
+				echo "$newline$f_tab<p>{$employee["name"]} from {$employee["company"]}<p>$newline";
+				echo "$f_tab<table>$newline$s_tab<tr>$newline" 
+				. "$t_tab<th>Day</th>$newline"
+				. "$t_tab<th>Shift</th>$newline "
+				. "$t_tab<th>Working</th>$newline"
+				. "$t_tab<th>Booked</th>$newline$s_tab</tr>$newline";
 				foreach ($employee["workingtimes"] as $dkey => $day){
 					foreach ($day as $skey => $shift){
-						echo "<tr> <td>$dkey</td> <td>$skey</td> <td>";
+						echo "$s_tab<tr>$newline"
+						. "$t_tab<td>$dkey</td>$newline$t_tab<td>$skey</td>$newline$t_tab<td>";
 						echo $shift["working"] ? "Yes" : "No";
-						echo "</td> <td>";
+						echo "</td>$newline$t_tab<td>";
 						echo $shift["booked"] ? "Yes" : "No";
-						echo "</td> </tr>\r\n";
+						echo "</td>$newline$s_tab</tr>$newline";
 					}
 				}
-				echo "</table>";
+				echo "$f_tab</table>$newline";
 			}
 			return true;
 		}
