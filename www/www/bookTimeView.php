49653ae57f720f828a3b1eebdacebb64ac753dfd
rmit-s3524307-Quang-Anh-Nguyen <s3524307@student.rmit.edu.au>
2017-04-09 23:33:38 +1000

    Update bookTimeView.php
    add changes for unit testing

--- www/www/bookTimeView.php
+++ www/www/bookTimeView.php
@@ -2,10 +2,15 @@
 include_once("../lib/JSONHandler.php");
 	
 $db = "../database/employeeWorkTime.json";
-$v = new View($db);
+$v = new View;
+if (!$v->viewFunction($db)){
+	echo "<p>"
+	. "Booking time file is either not found or corrupted"
+	. "<p>";
+}
 
 class View{
-	public function view($db){
+	public function viewFunction($db){
 		$j = new JSONHandler;
 		$f_tab = "\t\t";
 		$s_tab = "\t\t\t";
@@ -13,7 +18,17 @@ class View{
 		$newline = "\r\n";
 		if (!empty($viewArray = $j->getFileContents($db))){
 			foreach ($viewArray as $employee){
-				echo "$newline$f_tab<p>{$employee["name"]} from {$employee["company"]}<p>$newline";
+				if (empty($employee["company"])
+				|| empty($employee["name"])
+				|| empty($employee["gender"])
+				|| empty($employee["address"])
+				|| empty($employee["phoneno"])){
+					return false;
+				}
+				echo "$newline"
+				. "$f_tab<p>"
+				. "{$employee["name"]} from {$employee["company"]}"
+				. "<p>$newline";
 				echo "$f_tab<table>$newline$s_tab<tr>$newline" 
 				. "$t_tab<th>Day</th>$newline"
 				. "$t_tab<th>Shift</th>$newline "
@@ -22,11 +37,15 @@ class View{
 				foreach ($employee["workingtimes"] as $dkey => $day){
 					foreach ($day as $skey => $shift){
 						echo "$s_tab<tr>$newline"
-						. "$t_tab<td>$dkey</td>$newline$t_tab<td>$skey</td>$newline$t_tab<td>";
+						. "$t_tab<td>$dkey</td>$newline"
+						. "$t_tab<td>$skey</td>$newline"
+						. "$t_tab<td>";
 						echo $shift["working"] ? "Yes" : "No";
-						echo "</td>$newline$t_tab<td>";
+						echo "</td>$newline"
+						. "$t_tab<td>";
 						echo $shift["booked"] ? "Yes" : "No";
-						echo "</td>$newline$s_tab</tr>$newline";
+						echo "</td>$newline"
+						. "$s_tab</tr>$newline";
 					}
 				}
 				echo "$f_tab</table>$newline";
