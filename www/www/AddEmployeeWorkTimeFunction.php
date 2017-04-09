02aaf0c91facf571efec476bac6951d6bd6419b5
rmit-s3524307-Quang-Anh-Nguyen <s3524307@student.rmit.edu.au>
2017-04-10 05:50:59 +1000

    Add files via upload
    semi fix

--- www/www/AddEmployeeWorkTimeFunction.php
+++ www/www/AddEmployeeWorkTimeFunction.php
@@ -3,40 +3,83 @@
 include("../lib/JSONHandler.php");
 
 	$l = new AddEmployeeWorkTimeFunctions;
-	if($l->addWorkTimes($_POST["name"], 
-					$_POST["monMorning"], $_POST["monNoon"], $_POST["monAfternoon"], $_POST["monEvening"],
-					$_POST["tueMorning"], $_POST["tueNoon"], $_POST["tueAfternoon"], $_POST["tueEvening"],
-					$_POST["wedMorning"], $_POST["wedNoon"], $_POST["wedAfternoon"], $_POST["wedEvening"],
-					$_POST["thuMorning"], $_POST["thuNoon"], $_POST["thuAfternoon"], $_POST["thuEvening"],
-					$_POST["friMorning"], $_POST["friNoon"], $_POST["friAfternoon"], $_POST["friEvening"]))
-	{
-		header("Location: ");		
+	$inputTime = array(
+	"monday" => array(
+		"morning" => $_POST["monMorning"],
+		"midday" => $_POST["monNoon"],
+		"afternoon" => $_POST["monAfternoon"],
+    	"night" => $_POST["monEvening"]),
+    "tuesday" => array(
+		"morning" => $_POST["tueMorning"],
+		"midday" => $_POST["tueNoon"],
+		"afternoon" => $_POST["tueAfternoon"],
+    	"night" => $_POST["tueEvening"]),
+    "wednesday"=> array(
+		"morning" => $_POST["wedMorning"],
+		"midday" => $_POST["wedNoon"],
+		"afternoon" => $_POST["wedAfternoon"],
+    	"night" => $_POST["wedEvening"]),
+    "thursday"=> array(
+		"morning" => $_POST["thuMorning"],
+		"midday" => $_POST["thuNoon"],
+		"afternoon" => $_POST["thuAfternoon"],
+    	"night" => $_POST["thuEvening"]),
+    "friday"=> array(
+		"morning" => $_POST["friMorning"],
+		"midday" => $_POST["friNoon"],
+		"afternoon" => $_POST["friAfternoon"],
+    	"night" => $_POST["friEvening"]),
+    "saturday"=> array(
+		"morning" => $_POST["satMorning"],
+		"midday" => $_POST["satNoon"],
+		"afternoon" => $_POST["satAfternoon"],
+    	"night" => $_POST["satEvening"]),
+    "sunday"=> array(
+		"morning" => $_POST["sunMorning"],
+		"midday" => $_POST["sunNoon"],
+		"afternoon" => $_POST["sunAfternoon"],
+    	"night" => $_POST["sunEvening"]));
+	if($l->addWorkTimes($_POST["name"], $inputTime))
+	{	
+		header("Location: addWorkTimeConfirmation.html");
 	} else {
 		header("Location: addEmployeeWorkTime.html");
 	}
 	
 class AddEmployeeWorkTimeFunctions{
-	public function addWorkTimes($name, $monMorning, $monNoon, $monAfternoon, $monEvening,
-										$tueMorning, $tueNoon, $tueAfternoon, $tueEvening,
-										$wedMorning, $wedNoon, $wedAfternoon, $wedEvening,
-										$thuMorning, $thuNoon, $thuAfternoon, $thuEvening,
-										$friMorning, $friNoon, $friAfternoon, $friEvening){
+	public function addWorkTimes($name, $inputTime){
 		$j = new JSONHandler;
-		$employeedb = "../database/employees.json";
 		$worktimedb = "../database/employeeWorkTime.json";
-		if (!empty($j->search($employeedb, "name", $name))){
-			$data = array("name" => $name, 
-					"monMorning" => $monMorning, "monNoon" => $monNoon,  "monAfternoon" => $monAfternoon, "monEvening" => $monEvening, 
-					"tueMorning" => $tueMorning, "tueNoon" => $tueNoon,  "tueAfternoon" => $tueAfternoon, "tueEvening" => $tueEvening, 
-					"wedMorning" => $wedMorning, "wedNoon" => $wedNoon,  "wedAfternoon" => $wedAfternoon, "wedEvening" => $wedEvening, 
-					"thuMorning" => $thuMorning, "thuNoon" => $thuNoon,  "thuAfternoon" => $thuAfternoon, "thuEvening" => $thuEvening, 
-					"friMorning" => $friMorning, "friNoon" => $friNoon,  "friAfternoon" => $friAfternoon, "friEvening" => $friEvening);
-			$j->addToFile($worktimedb, $data);
-			return true;
-		} else {
-			//If employee name does not match
-			return false;
+		$edited = false;
+		if (!empty($viewArray = $j->getFileContents($worktimedb))){
+			foreach ($viewArray as $e_key => $employee){
+				if ($employee["name"]==$name){
+					foreach ($employee["workingtimes"] as $dkey => $day){
+						foreach ($inputTime as $in_dkey => $in_day){
+							if ($in_dkey==$dkey){
+								foreach ($day as $skey => $shift){
+									foreach ($in_day as $tkey => $value){
+										if ($tkey==$skey){
+											if ($value=="true"){
+												$viewArray[$e_key]["workingtimes"][$dkey][$skey]["working"] = true;
+											} else {
+												$viewArray[$e_key]["workingtimes"][$dkey][$skey]["working"] = false;
+											}
+											$edited = true;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		}
+		if ($edited){
+			$json = json_encode($viewArray, JSON_PRETTY_PRINT);
+			file_put_contents($worktimedb, $json);
+		}
+		return $edited;
 	}
 }
 
