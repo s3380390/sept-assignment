1e361edb4dd4f76e94d61a6faa07fe563134056e
siaueu96 <s3649170@student.rmit.edu.au>
2017-04-09 18:03:36 +1000

    modified for working times format

--- www/www/AddEmployeeFunction.php
+++ www/www/AddEmployeeFunction.php
@@ -14,8 +14,44 @@ class AddEmployeeFunctions{
 	public function addEmployee($firstname, $lastname, $gender, $address, $contact){
 		$j = new JSONHandler;
 		$employeedb = "../database/employees.json";
-		$data = array("name" => $firstname." ".$lastname, "gender" => $gender,
-							"address" => $address, "phoneno" => $contact);
+		$data = array("company" => $_SESSION['UserName'], "name" => $firstname." ".$lastname, "gender" => $gender,
+							"address" => $address, "phoneno" => $contact, "workingtimes" => 
+									["monday" => 
+										["morning" => ["working" => "false", "booked" => "false"],
+										 "midday" => ["working" => "false", "booked" => "false"],
+										 "afternoon" => ["working" => "false", "booked" => "false"],
+										 "night" => ["working" => "false", "booked" => "false"]],
+									"tuesday" => 
+										["morning" => ["working" => "false", "booked" => "false"],
+										 "midday" => ["working" => "false", "booked" => "false"],
+										 "afternoon" => ["working" => "false", "booked" => "false"],
+										 "night" => ["working" => "false", "booked" => "false"]],
+									"wednesday" => 
+										["morning" => ["working" => "false", "booked" => "false"],
+										 "midday" => ["working" => "false", "booked" => "false"],
+										 "afternoon" => ["working" => "false", "booked" => "false"],
+										 "night" => ["working" => "false", "booked" => "false"]],
+									"thursday" => 
+										["morning" => ["working" => "false", "booked" => "false"],
+										 "midday" => ["working" => "false", "booked" => "false"],
+										 "afternoon" => ["working" => "false", "booked" => "false"],
+										 "night" => ["working" => "false", "booked" => "false"]],
+									"friday" => 
+										["morning" => ["working" => "false", "booked" => "false"],
+										 "midday" => ["working" => "false", "booked" => "false"],
+										 "afternoon" => ["working" => "false", "booked" => "false"],
+										 "night" => ["working" => "false", "booked" => "false"]],
+									"saruday" => 
+										["morning" => ["working" => "false", "booked" => "false"],
+										 "midday" => ["working" => "false", "booked" => "false"],
+										 "afternoon" => ["working" => "false", "booked" => "false"],
+										 "night" => ["working" => "false", "booked" => "false"]],
+									"sunday" => 
+										["morning" => ["working" => "false", "booked" => "false"],
+										 "midday" => ["working" => "false", "booked" => "false"],
+										 "afternoon" => ["working" => "false", "booked" => "false"],
+										 "night" => ["working" => "false", "booked" => "false"]]]
+									);
 		$j->addToFile($employeedb, $data);
 		return true;
 		
