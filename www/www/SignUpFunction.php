72210082c14826eddff81fb946d2cbeeac7ea422
Matthew Williams <s3380390@student.rmit.edu.au>
2017-04-30 12:44:19 +1000

    SignUpFunction using config and logger

--- www/www/SignUpFunction.php
+++ www/www/SignUpFunction.php
@@ -1,30 +1,37 @@
 <?php
 	session_start();
-	include("../lib/JSONHandler.php");
+	include("config.php");
 	
 	$pattern = "/(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9])(?=.*[a-z])(?!.*[<>()?.,\/\\{}\[\\s]+\-~|;:\'\"=]).{6,}/";
 	if (preg_match($pattern, $_POST["password1"])){
+		$log->addDebug("Password matches format restrictions");
 		if ($_POST["password1"]==$_POST["password2"]){
 			$l = new SignupFunctions;
-			if($l->signup($_POST["firstname"], $_POST["lastname"], $_POST["username"], $_POST["password1"], 
-			$_POST["password2"], $_POST["contact"], $_POST["address"])==true){
+			if($l->signup($database["customers"], $_POST["firstname"], $_POST["lastname"], $_POST["username"], 
+			$_POST["password1"], $_POST["password2"], $_POST["contact"], $_POST["address"])==true){
+				$log->addInfo("Sign up success for " . $_POST["username"]);
 				header("Location: SignUpConfirmation.html");		
 			} else {
+				$log->addInfo("Sign up failed for " . $_POST["username"] . ", user already exists");
 				$_SESSION["sameUserName"] = true;
 				header("Location: register.html");
 			}		
 		} else {
+			$log->addInfo("Sign up failed, passwords do not match");
 			$_SESSION["samePwd"] = false;
 			header("Location: register.html");
 		}
 	} else {
+		$log->addInfo("Sign up failed, passwords does not match format restrictions");
 		$_SESSION["format"] = false;
 		header("Location: register.html");
 	}
+
 class SignupFunctions{
-	public function signup($firstname, $lastname, $username, $password1, $password2, $contact, $address){
+	
+	public function signup($customersdb, $firstname, $lastname, $username, $password1, $password2, $contact, $address){
 		$j = new JSONHandler;
-		$customersdb = "../database/customers.json";
+		//$customersdb = "../database/customers.json";
 		if ($password1 == $password2){
 			if (!empty($j->search($customersdb, "username", $username))){
 				return false;
