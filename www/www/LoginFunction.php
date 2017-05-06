e2be3b1e8cd70547a0ab394a0d1d2cd2224e7cbe
rmit-s3575527-anthony-kasiyazi <s3575527@student.rmit.edu.au>
2017-05-06 14:24:31 +1000

    Update LoginFunction.php

--- www/www/LoginFunction.php
+++ www/www/LoginFunction.php
@@ -4,10 +4,12 @@ include_once("config.php");
 	
 	$l = new LoginFunctions;
 	
+         //Checking if user login in details are in the customer database
 	if($l->login($_POST["username"], $_POST["password"], $database['customers'], "customer")){
 		$log->addInfo("Customer: " . $_POST["username"] . " logged in successfully");
 		header("Location: CustomerHome.html");
 	} else {
+		//Checking if user login in details are in the owner database
 		if ($l->login($_POST["username"], $_POST["password"], $database['owners'], "owner")){
 			$log->addInfo("Owner: " . $_POST["username"] . " logged in successfully");
 			header("Location: OwnerHome.html");
@@ -20,10 +22,13 @@ include_once("config.php");
 class LoginFunctions{
 	public function login($username, $password, $db, $type){
 		$j = new JSONHandler;
+		//search for username in database
 		if (!empty($user = $j->search($db, "username", $username))){
+			//if username is found, checking if password matches
 			if ($user["password"] == $password){
 				$_SESSION["UserName"] = $user["username"];
 				$_SESSION["Password"] = $user["password"];
+				//distinguishing between customer or owner
 				if ($type == "customer"){
 					$_SESSION["Name"] = $user["name"];
 					$_SESSION["UserType"] = "Customer";
